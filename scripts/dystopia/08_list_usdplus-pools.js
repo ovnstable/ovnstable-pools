eca4b5268675600aeacdf1391cc02fb94d0cd0af
Мосин Николай Григорьевич <Mosin.N.G@sberbank.ru>
2022-05-16 01:08:22 +0300

    add list pool script for dyst and mesh

--- scripts/dystopia/08_list_usdplus-pools.js
+++ scripts/dystopia/08_list_usdplus-pools.js
@@ -11,30 +11,32 @@ let UsdPlusToken = JSON.parse(fs.readFileSync('./abi/UsdPlusToken.json'));
 let iUniswapV2FactoryAbi = JSON.parse(fs.readFileSync('./abi/build/IUniswapV2Factory.json')).abi;
 let iUniswapV2PairAbi = JSON.parse(fs.readFileSync('./abi/build/IUniswapV2Pair.json')).abi;
 
-let qsFactoryAddress = "0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32";
+let dystFactoryAddress = "0x1d21Db6cde1b18c7E47B0F7F42f4b3F68b9beeC9";
 
 
 async function main() {
 
     let wallet = await initWallet(ethers);
-    let qsFactory = await ethers.getContractAt(iUniswapV2FactoryAbi, qsFactoryAddress, wallet);
+    let dystFactory = await ethers.getContractAt(iUniswapV2FactoryAbi, dystFactoryAddress, wallet);
 
     let usdPlusAddress = UsdPlusToken.address.toString().toLowerCase();
 
 
-    let count = new BN((await qsFactory.allPairsLength()).toString());
-    console.log(`Total pools on QS: ${count}`);
+    let count = new BN((await dystFactory.allPairsLength()).toString());
+    console.log(`Total pools on Dyst: ${count}`);
 
     let dir = 'pool_infos'
     makeDirIfNeed(dir);
 
-    let poolInfosFileName = `${dir}/qsPools.json`
-    let usdPlusPoolInfosFileName = `${dir}/qsPools_usdPlus.json`
+    let poolInfosFileName = `${dir}/dystopiaPools.json`
+    let usdPlusPoolInfosFileName = `${dir}/dystopiaPools_usdPlus.json`
 
     let poolInfos = {};
     if (fs.existsSync(poolInfosFileName)) {
         poolInfos = JSON.parse(fs.readFileSync(poolInfosFileName, "utf8"));
     }
+
+
 
     let storeEvery = 0;
     let storeEveryCounter = 0;
@@ -45,7 +47,7 @@ async function main() {
 
         let prevInfo = poolInfos[index];
         if (prevInfo !== undefined) {
-            console.log(`pool!: ${prevInfo.index} -> ${prevInfo.pool}: ${prevInfo.token0Symbol}/${prevInfo.token1Symbol} : ${prevInfo.token0}/${prevInfo.token1}`)
+            console.log(`loaded info pool: ${prevInfo.index} -> ${prevInfo.pool}: ${prevInfo.token0Symbol}/${prevInfo.token1Symbol} : ${prevInfo.token0}/${prevInfo.token1}`)
             index = index.subn(1);
             continue
         }
@@ -56,7 +58,7 @@ async function main() {
                 (async () => {
                     let localIndex = index.toString();
 
-                    let poolAddress = await qsFactory.allPairs(localIndex);
+                    let poolAddress = await dystFactory.allPairs(localIndex);
                     let pool = await ethers.getContractAt(iUniswapV2PairAbi, poolAddress, wallet);
                     let poolInfo = await getPoolInfo(pool, localIndex);
 
@@ -71,12 +73,12 @@ async function main() {
         }
 
         for (const poolInfo of await Promise.all(promises)) {
-            console.log(`pool: ${poolInfo.index} -> ${poolInfo.pool}: ${poolInfo.token0Symbol}/${poolInfo.token1Symbol} : ${poolInfo.token0}/${poolInfo.token1}`)
+            console.log(`load info pool: ${poolInfo.index} -> ${poolInfo.pool}: ${poolInfo.token0Symbol}/${poolInfo.token1Symbol} : ${poolInfo.token0}/${poolInfo.token1}`)
 
             poolInfos[poolInfo.index] = poolInfo;
         }
 
-        // let poolAddress = await qsFactory.allPairs(index.toString());
+        // let poolAddress = await dystFactory.allPairs(index.toString());
         //
         // let pool = await ethers.getContractAt(iUniswapV2PairAbi, poolAddress, wallet);
         // let poolInfo = await getPoolInfo(pool, index);
@@ -101,7 +103,7 @@ async function main() {
 
     let poolInfos_usdPlus = {};
 
-    console.log(`--- QuickSwap usdPlus pools:`)
+    console.log(`--- dystopia usdPlus pools:`)
 
     index = count.subn(1);
     while (index.gte(zero)) {
